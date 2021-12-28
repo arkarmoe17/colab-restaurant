a50c814a9a4f966b1160d779547e769b882c95b3
arkarmoe17 <arkarmoe17@gmail.com>
2021-12-28 16:19:11 +0630

    access,refresh token expire time

--- src/main/java/com/arkarmoe/springbootjwt/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/arkarmoe/springbootjwt/filter/CustomAuthenticationFilter.java
@@ -63,14 +63,14 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         Algorithm algorithm = Algorithm.HMAC256(Constant.Token.SECRET.getBytes());
         String access_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + Constant.Token.ACCESS_TOKEN_EXPIRE))
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim(Constant.Token.ROLES, user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
 
         String refresh_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + Constant.Token.REFRESH_TOKEN_EXPIRE))
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
 

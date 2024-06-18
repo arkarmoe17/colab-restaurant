da0e37b2e74fc2cb1b6b959f6cc1802b23d591d0
arkarmoe <arkarmoedev1994@gmail.com>
2024-06-18 20:52:44 +0700

    api tag with swagger

--- src/main/java/com/colab/restaurant/controller/OwnerController.java
+++ src/main/java/com/colab/restaurant/controller/OwnerController.java
@@ -4,6 +4,8 @@ import com.colab.restaurant.mapper.OwnerMapper;
 import com.colab.restaurant.model.dto.OwnerDTO;
 import com.colab.restaurant.model.entity.Owner;
 import com.colab.restaurant.service.OwnerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +15,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1.0/owners")
 @RequiredArgsConstructor
+@Tag(name = "Owner", description = "Owner API")
 public class OwnerController {
     private final OwnerService ownerService;
 
     // create
+    @Operation(summary = "Owner Creation")
     @PostMapping
     public ResponseEntity<Void> createOwner(@RequestBody OwnerDTO dto) {
         Owner owner = OwnerMapper.INSTANCE.toEntity(dto);
@@ -25,6 +29,7 @@ public class OwnerController {
     }
 
     // find by shop code
+    @Operation(summary = "Fetching the owners by shopCode")
     @GetMapping
     public ResponseEntity<List<OwnerDTO>> findAllByShopCode(@RequestHeader("shop-code") String shopCode) {
         return ResponseEntity.ok(ownerService.findAllByShopCode(shopCode));
--- src/main/java/com/colab/restaurant/controller/RestaurantController.java
+++ src/main/java/com/colab/restaurant/controller/RestaurantController.java
@@ -2,6 +2,8 @@ package com.colab.restaurant.controller;
 
 import com.colab.restaurant.model.dto.RestaurantDTO;
 import com.colab.restaurant.service.RestaurantService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,29 +12,34 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1.0/restaurants")
+@Tag(name = "Restaurant", description = "Restaurant API")
 @RequiredArgsConstructor
 public class RestaurantController {
     private final RestaurantService restaurantService;
 
     // Fetch Restaurants
+    @Operation(summary = "Fetch all the restaurants")
     @GetMapping
     public ResponseEntity<List<RestaurantDTO>> fetchAllRestaurants() {
         return ResponseEntity.ok(restaurantService.fetchAllRestaurants());
     }
 
     // Fetch Shop Codes
+    @Operation(summary = "Fetch all shopCodes")
     @GetMapping("/shopCodes")
     public ResponseEntity<List<String>> fetchAllActiveShopCodes() {
         return ResponseEntity.ok(restaurantService.fetchAllActiveShopCodes());
     }
 
     // Register Restaurant
+    @Operation(summary = "Register the restaurant")
     @PostMapping
     public ResponseEntity<Void> registerRestaurant(@RequestBody RestaurantDTO requestDTO) {
         restaurantService.registerRestaurant(requestDTO);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Update the restaurant")
     @PutMapping
     public ResponseEntity<RestaurantDTO> updateRestaurant(@RequestHeader("restaurant-id") String restaurantId,
                                                           @RequestBody RestaurantDTO requestDTO) {
